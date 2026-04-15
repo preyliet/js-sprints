d0d41309d6f1b91553c3b9a2dd2a9984db6e232c
rohannilatkar <iix.preyliet@gmail.com>
2026-04-15 23:12:54 +0530

    Update maths.js

--- maths.js
+++ maths.js
@@ -1,1 +1,11 @@
+2 + 2 * 2
+console.log("here the 2 * 2 is calculated first and then addition is calculated")
+// same goes for division
+// Orders od operations 
+* and / are done first
++ and - are done after
 
+here the * and / have the same priority and it is then calculated from left to right.
+for eg, 2 * 3 / 5
+
+same goes for addition and subtraction it goes from left to right.
