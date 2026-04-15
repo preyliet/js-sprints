e0707953ead493e2824d5ab9410cd410b1a49c3a
rohannilatkar <iix.preyliet@gmail.com>
2026-04-15 23:16:56 +0530

    Update maths.js

--- maths.js
+++ maths.js
@@ -9,3 +9,15 @@ here the * and / have the same priority and it is then calculated from left to r
 for eg, 2 * 3 / 5
 
 same goes for addition and subtraction it goes from left to right.
+
+Now the calculate the persentage for example tax (%) like 10 % on a product
+
+1 % = 1 / 100 , where one is the per and 100 is the cent
+10% = 10/100 = 0.1
+
+And we use brackets to calculate a equation first if needed. 
+
+for eg, (20.95 + 7.99 * 2) * 0.1
+we get, = 3.693
+
+
