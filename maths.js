f82722e34cb848d6713f5d85c1e8a6e23cc74a8d
rohannilatkar <iix.preyliet@gmail.com>
2026-04-22 23:29:45 +0530

    Enhance comments for percentage and tax calculations
    Added explanation for calculating percentage and tax.

--- maths.js
+++ maths.js
@@ -21,5 +21,12 @@ for eg, (20.95 + 7.99 * 2) * 0.1
 we get, = 3.693
 
 now calculating in cents for a accurate answer.
+
+Now to calculate percentage or tax 
+we multiplyby 0.1 for suppose 10% tax on any product.
+
+Because 1% = 1/100 where 1 is per and 100 cent
+
+10% = 10/100 = 0.1
          
 
